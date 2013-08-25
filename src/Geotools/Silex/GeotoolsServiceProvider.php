9093dc877dd30aedfc3ffb7de258cc8c6ad7ef7b
Antoine Corcy <dev@sbin.dk>
2013-08-25 16:57:14 +0200

    fix stupid error

--- src/Geotools/Silex/GeotoolsServiceProvider.php
+++ src/Geotools/Silex/GeotoolsServiceProvider.php
@@ -30,7 +30,7 @@ class GeotoolsServiceProvider implements ServiceProviderInterface
             return new Geotools;
         });
 
-        $this->registerConsole();
+        $this->registerConsole($app);
     }
 
     /**
